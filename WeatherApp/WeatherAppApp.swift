223b99765c5c90487ed6121e29da85278b4529be
Lahiru Amarasooriya <lahirudx@gmail.com>
2023-10-06 17:29:08 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Lahiru Amarasooriya on 2023-10-06.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
